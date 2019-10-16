227bb13818fbe59eb89809bd18cbfdfb70870dd4
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-10-16 19:03:03 +0600

    Wed, Oct 16, 2019  7:03:02 PM

--- app/src/main/java/hridoy/aiz/simplerssexample/MainActivity.java
+++ app/src/main/java/hridoy/aiz/simplerssexample/MainActivity.java
@@ -2,13 +2,75 @@ package hridoy.aiz.simplerssexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvRss;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvRss = findViewById(R.id.lvRss);
+
+        lvRss.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    public InputStream getIInputStream(URL url) {
+
+        try{
+            url.openConnection().getInputStream();
+        }catch(IOException e){
+            exception = e;
+            return null;
+        }catch (XmlPullParserException e){
+
+        }
+
+    }
+
+    public class ProcessInBckground extends AsyncTask<Integer,Void,String> {
+        ProgressDialog pd = new ProgressDialog(MainActivity.this);
+        Exception excepiton = null;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setMessage("busy loading");
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+
+            try{
+
+            }catche ()
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
